bf10ebb5b69cab52245d5b600440f81953409ee3
jaesung <jaesung.kim@riiid.co>
2020-10-15 21:16:44 +0900

    점검 모드 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-const { BASE_URL, NEXTAUTH_URL } = process.env;
+const { BASE_URL, NEXTAUTH_URL, ON_MAINTENANCE } = process.env;
 
 module.exports = {
   env: {
     BASE_URL: BASE_URL || NEXTAUTH_URL,
+    ON_MAINTENANCE: ON_MAINTENANCE === 'true' ? true : false,
   }
 }
---
+++ src/pages/maintenance.tsx
@@ -0,0 +1,5 @@
+const MaintenancePage = () => {
+  return <div>서비스 점검 중입니다...</div>;
+}
+
+export default MaintenancePage
